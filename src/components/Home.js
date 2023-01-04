b4d96d78e2a793236bf92c06b81aa4427138fdeb
Ogaga <iyaraogaga69@gmail.com>
2023-01-04 06:27:09 +0100

    Add jsx for homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,35 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchAllCountries } from '../store/fetch-actions';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllCountries } from "../store/fetch-actions";
 
 const Home = () => {
-    const dispatch = useDispatch()
+  const countries = useSelector((state) => {
+    return state.regionSlice.countries;
+  });
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(fetchAllCountries())
-    })
+  useEffect(() => {
+    dispatch(fetchAllCountries());
+  });
 
-    return (
-        <div>
-
-        </div>
-    );
+  return (
+    <>
+      {countries.map((country) => {
+        return (
+          <div className={styles["country-card"]}>
+            <div className={styles.flag}>
+              <img src={country.flag} alt="flag" />
+            </div>
+            <div className={styles["country-info"]}>
+              <h1 className={styles.name}>{country.name}</h1>
+              <h3 className={styles.capital}>{country.capital}</h3>
+              <h5 className={styles.continent}>{country.continent}</h5>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Home;
